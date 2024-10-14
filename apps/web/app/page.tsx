7f27e2600b595df8f810125e0f08119a1dca1333
Anish Araz <anisharaz919@gmail.com>
2024-10-14 16:39:46 +0530

    INIT: shadcn UI in web

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,13 +1,9 @@
-import { Button } from "@repo/ui";
-import { PG_PRISMA_CLIENT, MONGO_PRISMA_CLIENT } from "@repo/database";
+import { Button } from "@/components/ui/button";
+// import { PG_PRISMA_CLIENT, MONGO_PRISMA_CLIENT } from "@repo/database"; // Example of importing Prisma clients
 export default async function Home() {
-  const user = await PG_PRISMA_CLIENT.user.findFirst();
-  const data = await MONGO_PRISMA_CLIENT.idea.findFirst();
   return (
     <div>
       <div className="text-red-500">Hello world!!</div>
-      <div>{user?.id}</div>
-      <div>{data?.id}</div>
       <Button>Button</Button>
     </div>
   );
