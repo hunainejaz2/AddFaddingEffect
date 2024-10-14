03e91f88b5d87621aab2853cdb5fde8e5193957a
Anish Araz <anisharaz919@gmail.com>
2024-10-14 11:24:20 +0530

    INIT:monorepo tailwindcss in web

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,6 +3,7 @@ import { Button } from "@repo/ui";
 export default function Home() {
   return (
     <div>
+      <div className="text-red-500">Hello world!!</div>
       <Button>Button</Button>
     </div>
   );
