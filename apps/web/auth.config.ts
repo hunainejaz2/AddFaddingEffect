e0eb179caec410a6566012484d0f5417050144b3
Anish Araz <anisharaz919@gmail.com>
2024-10-14 20:36:11 +0530

    FEAT: added next-auth authentication

---
+++ apps/web/auth.config.ts
@@ -0,0 +1,27 @@
+import type { NextAuthConfig } from "next-auth";
+import google from "next-auth/providers/google";
+
+export default {
+  providers: [google],
+  trustHost: true,
+  callbacks: {
+    // this function runs on each and every request according to the matcher in middleware file
+    authorized: async ({ auth, request: { nextUrl } }) => {
+      /* 
+        TODO: Uncomment This and add the code to protect routes in the similar way as in the example below
+      */
+      // const isLoggedIn = !!auth?.user;
+      // const isOnUser = nextUrl.pathname.startsWith("/user");
+      // if (isOnUser) {
+      //   if (isLoggedIn) {
+      //     return true;
+      //   }
+      //   return false;
+      // }
+      return true;
+    },
+  },
+  // theme: {
+  //   logo: "/logo.svg",
+  // },
+} satisfies NextAuthConfig;
---
+++ apps/web/auth.ts
@@ -0,0 +1,15 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import NextAuth from "next-auth";
+import authConfig from "./auth.config";
+import { PG_PRISMA_CLIENT } from "@repo/database";
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(PG_PRISMA_CLIENT),
+  session: { strategy: "jwt", maxAge: 7 * 24 * 60 * 60 },
+  ...authConfig,
+  // callbacks: {
+  //   // this function determines if the user is allowed to signing or not, it runs before the user is created in the database
+  //   async signIn({ user, account, profile, email, credentials }) {
+  //     return true;
+  //   },
+  // },
+});
