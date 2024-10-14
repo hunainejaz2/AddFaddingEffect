ac1bc3b6a917dd36f7c0d3859c42b4708001a8a3
Anish Araz <anisharaz919@gmail.com>
2024-10-14 23:07:14 +0530

    FIX: same shadcn theme in web & UI package

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import "@repo/ui/css";
+// import "@repo/ui/css"; // This is needed only if the importing project is not using shadcn ui
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,4 @@
-import { Button } from "@/components/ui/button";
 // import { PG_PRISMA_CLIENT, MONGO_PRISMA_CLIENT } from "@repo/database"; // Example of importing Prisma clients
 export default async function Home() {
-  return (
-    <div>
-      <div className="text-red-500">Hello world!!</div>
-      <Button>Button</Button>
-    </div>
-  );
+  return <div>HomePage</div>;
 }
